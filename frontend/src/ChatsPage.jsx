95837dcea1ac90f166ddf624590e8af02e1ff76c
Huseyin Taze <huseyintaze105@gmail.com>
2023-09-12 20:51:23 +0300

    using pretty template

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,15 +1,14 @@
-import {MultiChatSocket,MultiChatWindow,useMultiChatLogic} from 'react-chat-engine-advanced'
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        '46dfd749-6edb-42f8-af2e-21b7924a7434',
-        props.user.username,
-        props.user.secret
-        )
     return (
     <div style={{height:'100vh'}}>
-        <MultiChatSocket {...chatProps}/>
-        <MultiChatWindow {...chatProps} style={{height:'100%'}}/>
+        <PrettyChatWindow 
+            projectId='46dfd749-6edb-42f8-af2e-21b7924a7434' 
+            username={props.user.username} 
+            secret={props.user.secret} 
+            style={{height:'100%'}}
+        />
     </div>
     )
 }
